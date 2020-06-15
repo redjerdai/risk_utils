880f8cf5f5f91d3f48bf9236c3d3849312e7085c
Edward Aziz <edazizovv@gmail.com>
2020-06-15 21:55:03 +0300

    SetUp Added

---
+++ setup.py
@@ -0,0 +1,22 @@
+#
+
+import setuptools
+
+
+setuptools.setup(
+    name="risk_utils",
+    version="0.1.0",
+    author="Edward Aziz",
+    author_email="edazizovv@gmail.com",
+    description="A naive risk machine package",
+    license='MIT',
+    long_description='',
+    long_description_content_type="text/markdown",
+    url="https://github.com/redjerdai/risk_utils/",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
